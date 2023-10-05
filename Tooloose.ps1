a1108b937680ef6ec943c605e808be5bfff100af
Niccolo Masaling <53284964+niccolo-bluedigital@users.noreply.github.com>
2023-10-05 14:50:21 +1300

    Add files via upload

---
+++ Tooloose.ps1
@@ -0,0 +1,21 @@
+﻿$Url = "https://aeimagesintune.blob.core.windows.net/backgroundimages/_AdminToolsV3.zip"
+
+$DownloadZipFile = "C:\temp\" + $(Split-Path -Path $Url -Leaf)
+
+$ExtractPath = "C:\temp\"
+
+Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
+
+$ExtractShell = New-Object -ComObject Shell.Application 
+
+$ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items() 
+
+$ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles) 
+
+Start-Process $ExtractPath
+
+Set-ExecutionPolicy -ExecutionPolicy bypass
+
+Set-Location C:\Temp\_AdminToolsV3
+
+.\NicoAdminV2.ps1
