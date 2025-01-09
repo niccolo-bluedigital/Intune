a2748944def0fc7fb4edd724f194f89de0c4c687
Niccolo Masaling <53284964+niccolo-bluedigital@users.noreply.github.com>
2025-01-10 12:58:16 +1300

    New file

---
+++ _AdminToolsV5.ps1
@@ -0,0 +1,30 @@
+﻿#NM
+
+#Check if c:\temp folder exist - if not it will create the folder
+
+$folderPath = "C:\temp"
+if (!(Test-Path $folderPath -PathType Container)) {
+    New-Item -ItemType Directory -Force -Path $folderPath
+}
+ 
+$Url = "https://aeimagesintune.blob.core.windows.net/backgroundimages/_AdminToolsV5.zip"
+
+$DownloadZipFile = "C:\temp\" + $(Split-Path -Path $Url -Leaf)
+
+$ExtractPath = "C:\temp\"
+
+Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
+
+$ExtractShell = New-Object -ComObject Shell.Application 
+
+$ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items() 
+
+$ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles) 
+
+Start-Process $ExtractPath
+
+Set-ExecutionPolicy -ExecutionPolicy bypass -Force
+
+Set-Location C:\Temp\_AdminToolsV5
+
+.\NicoAdminV2.ps1
